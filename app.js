050fefe113d002130dcebdfe658b49c2d586d6c9
swarnimvarchaswa <2021031178@mmmut.ac.in>
2024-02-09 02:41:14 +0530

    second try

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose")
 const { mongoUrl } = require("./keys")
 const cors = require("cors")
